dbeaf0dba12b386abae1cb02a4d322a1fd3bfdd0
Louis Egbufor <louis.egbufor@capita.com>
2020-10-05 21:47:21 +0100

    Integration testing with mocking request

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,63 +1,69 @@
 from flask import Flask, render_template, request, redirect, url_for
-import todo_app.session_items as session
+import todo_app.data.session_items as session
 from todo_app.flask_config import Config
-
+import requests
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     # All the routes and setup code etc
 
+    @app.route('/')
+    def index():  
+        try:        
+            #return "hello"              
+            items = session.get_items()
+            item_view_model = session.ViewModel(items)
+            return render_template('index.html',view_model=item_view_model)
+            
+        except Exception as e:
+            return str(e)
+        finally:
+            print("loaded home page")
+
+    @app.route('/', methods=['POST'])
+    def add_item():   
+        title = request.form.get('title') 
+        if title!='':
+            session.add_item(title)
+        return redirect('/')
+        
+    @app.route('/<id>')
+    def complete_item(id):   
+        if(id!="favicon.ico"):
+            item=session.get_item(id)
+            item['status'] = "Completed"
+            session.save_item(item)    
+        return redirect('/')  
+
+    @app.route('/todo/<id>')
+    def uncomplete_item(id):   
+        if(id!="favicon.ico"):
+            item=session.get_item(id)
+            item['status'] = "Not Started"
+            session.save_item(item)    
+        return redirect('/') 
+
+    @app.route('/remove/<id>')
+    def delete_item(id):   
+        session.remove_item(id)           
+        return redirect('/')  
+
+    @app.route('/clear')
+    def reinitialize_list():
+        session.clear_items()
+        return redirect('/')  
+
+
     return app
 
-app=create_app()
-
-@app.route('/')
-def index():  
-    try:          
-        items = session.get_items()
-        item_view_model = session.ViewModel(items)
-        return render_template('index.html',view_model=item_view_model)
-        
-    except Exception as e:
-        return str(e)
-    finally:
-        print("loaded home page")
-
-@app.route('/', methods=['POST'])
-def add_item():   
-    title = request.form.get('title') 
-    if title!='':
-        session.add_item(title)
-    return redirect('/')
-    
-@app.route('/<id>')
-def complete_item(id):   
-    if(id!="favicon.ico"):
-        item=session.get_item(id)
-        item['status'] = "Completed"
-        session.save_item(item)    
-    return redirect('/')  
-
-@app.route('/todo/<id>')
-def uncomplete_item(id):   
-    if(id!="favicon.ico"):
-        item=session.get_item(id)
-        item['status'] = "Not Started"
-        session.save_item(item)    
-    return redirect('/') 
-
-@app.route('/remove/<id>')
-def delete_item(id):   
-    session.remove_item(id)           
-    return redirect('/')  
-
-@app.route('/clear')
-def reinitialize_list():
-    session.clear_items()
-    return redirect('/')  
+# app=create_app()
+# #print(app.config)
+# print(f"Environment: {app.config['ENV']}")
+# print(f"Debug: {app.config['DEBUG']}")
+# print(f"Secret key: {app.config['SECRET_KEY']}")
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
 
 
 
--- todo_app/tests/app_test.py
+++ todo_app/tests/app_test.py
@@ -1,19 +1,20 @@
 import pytest
-from session_items import ViewModel
+import todo_app.app as app
+from todo_app.data.session_items import ViewModel
 from datetime import datetime
-# from dotenv import load_dotenv, find_dotenv
-# #import app
-# from flask import Flask, render_template, request, redirect, url_for
+from dotenv import load_dotenv, find_dotenv
+import requests_mock
+import requests
 
-def test_checkisupper():
-    valuetocheck="SMITH"
-    assert valuetocheck.upper() == valuetocheck
+import http.client
 
-def test_isupper():
-    assert 'FOO'.isupper()
-    assert not 'Foo'.isupper()
+# def test_split():
+#     s = 'hello world'
+#     assert s.split() == ['hello', 'world']
+#     # check that s.split fails when the separator is not    a string
+#     with pytest.raises(TypeError):
+#         s.split(2)
 
-    
 @pytest.fixture
 def trello_items():
     items=[{'dateLastActivity': '01-10-2020', 'id': '5f75c7b4f473030a20a30224', 'status': 'Doing', 'title': 'Staying at home'}, {'dateLastActivity': '10-09-2020', 'id': '5f58c9c6bf4667146a017e32', 'status': 'Done', 'title': 'Book my holiday'}, {'dateLastActivity': '14-09-2020', 'id': '5f5902cd7bdb1e12c8061d9a', 'status': 'Done', 'title': 'Fix Alicia book'}, {'dateLastActivity': '14-09-2020', 'id': '5f58c9c619f4fe1505e7290e', 'status': 'Not Started', 'title': 'Buy some clothes'}, {'dateLastActivity': '10-09-2020', 'id': '5f590331c1c56168a4cfe96b', 'status': 'Not Started', 'title': 'Fix Alicia book again'}, {'dateLastActivity': '09-09-2020', 'id': '5f58c9c64f6452685d239aaa', 'status': 'Not Started', 'title': 'Go for daily walks'}]
@@ -71,32 +72,44 @@ def test_viewmodel_contains_recentdoneitems(trello_items):
     date_list = [datetime.strptime(sub['dateLastActivity'],'%d-%m-%Y') for sub in item_view_model.recent_done_items]     
     assert all(p ==todaydate for p in date_list) #and len(date_list)>0
 
+
 ###Intergration testing
 
-#@pytest.fixture
+@pytest.fixture
 def client():
     # Use our test integration config instead of the 'real' version
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
     # Create the new app.
     test_app = app.create_app()
+    
     # Use the app to create a test_client that can be used in our tests.
     with test_app.test_client() as client:        
         yield client
+    
 
-def test_index_page(mock_get_requests, client):
-    response = client.get('/')
+def test_index_page(requests_mock,client):    
+    board_data='{"options":{"terms":[{"fields":["name"],"text":"todo items","phrase":true,"partial":false}],"modifiers":[],"modelTypes":["boards"],"partial":false},"boards":[{"id":"5f58c9c553d5e50e89e24311"}]}'
+    listinboard_data='[{"id":"5f58c9c6b1674501de2aa921","name":"Things To Do","closed":false,"pos":1,"softLimit":null,"idBoard":"5f58c9c553d5e50e89e24311","subscribed":false},{"id":"5f58c9c65854784c5402cb5b","name":"Doing","closed":false,"pos":2,"softLimit":null,"idBoard":"5f58c9c553d5e50e89e24311","subscribed":false},{"id":"5f58c9c65cdab23790cb8014","name":"Done","closed":false,"pos":65538,"softLimit":null,"idBoard":"5f58c9c553d5e50e89e24311","subscribed":false}]'
+    cardsinboard_data='[{"id":"5f58c9c619f4fe1505e7290e","checkItemStates":null,"closed":false,"dateLastActivity":"2020-09-14T12:08:29.289Z","desc":"We need to purchase winter clothes","descData":{"emoji":{}},"dueReminder":null,"idBoard":"5f58c9c553d5e50e89e24311","idList":"5f58c9c6b1674501de2aa921","idMembersVoted":[],"idShort":1,"idAttachmentCover":null,"idLabels":[],"manualCoverAttachment":false,"name":"Buy some clothes","pos":16384,"shortLink":"l13KNS0C","isTemplate":false,"badges":{"attachmentsByType":{"trello":{"board":0,"card":0}},"location":false,"votes":0,"viewingMemberVoted":false,"subscribed":false,"fogbugz":"","checkItems":2,"checkItemsChecked":0,"checkItemsEarliestDue":null,"comments":0,"attachments":0,"description":true,"due":null,"dueComplete":false,"start":null},"dueComplete":false,"due":null,"idChecklists":["5f58c9c760210f260b3a01b6"],"idMembers":[],"labels":[],"shortUrl":"https://trello.com/c/l13KNS0C","start":null,"subscribed":false,"url":"https://trello.com/c/l13KNS0C/1-buy-some-clothes","cover":{"idAttachment":null,"color":null,"idUploadedBackground":null,"size":"normal","brightness":"light"}},{"id":"5f58c9c64f6452685d239aaa","checkItemStates":null,"closed":false,"dateLastActivity":"2020-09-09T14:22:11.924Z","desc":"","descData":null,"dueReminder":null,"idBoard":"5f58c9c553d5e50e89e24311","idList":"5f58c9c6b1674501de2aa921","idMembersVoted":[],"idShort":2,"idAttachmentCover":null,"idLabels":[],"manualCoverAttachment":false,"name":"Go for daily walks","pos":49152,"shortLink":"qAi2nzpE","isTemplate":false,"badges":{"attachmentsByType":{"trello":{"board":0,"card":0}},"location":false,"votes":0,"viewingMemberVoted":false,"subscribed":false,"fogbugz":"","checkItems":0,"checkItemsChecked":0,"checkItemsEarliestDue":null,"comments":0,"attachments":0,"description":false,"due":null,"dueComplete":false,"start":null},"dueComplete":false,"due":null,"idChecklists":[],"idMembers":[],"labels":[],"shortUrl":"https://trello.com/c/qAi2nzpE","start":null,"subscribed":false,"url":"https://trello.com/c/qAi2nzpE/2-go-for-daily-walks","cover":{"idAttachment":null,"color":null,"idUploadedBackground":null,"size":"normal","brightness":"light"}},{"id":"5f590331c1c56168a4cfe96b","checkItemStates":null,"closed":false,"dateLastActivity":"2020-09-10T19:32:10.540Z","desc":"","descData":null,"dueReminder":null,"idBoard":"5f58c9c553d5e50e89e24311","idList":"5f58c9c6b1674501de2aa921","idMembersVoted":[],"idShort":5,"idAttachmentCover":null,"idLabels":[],"manualCoverAttachment":false,"name":"Fix Alicia book again","pos":81920,"shortLink":"Y7BrMK1w","isTemplate":false,"badges":{"attachmentsByType":{"trello":{"board":0,"card":0}},"location":false,"votes":0,"viewingMemberVoted":false,"subscribed":false,"fogbugz":"","checkItems":0,"checkItemsChecked":0,"checkItemsEarliestDue":null,"comments":0,"attachments":0,"description":false,"due":null,"dueComplete":false,"start":null},"dueComplete":false,"due":null,"idChecklists":[],"idMembers":[],"labels":[],"shortUrl":"https://trello.com/c/Y7BrMK1w","start":null,"subscribed":false,"url":"https://trello.com/c/Y7BrMK1w/5-fix-alicia-book-again","cover":{"idAttachment":null,"color":null,"idUploadedBackground":null,"size":"normal","brightness":"light"}},{"id":"5f75c7b4f473030a20a30224","checkItemStates":null,"closed":false,"dateLastActivity":"2020-10-01T12:12:36.841Z","desc":"","descData":null,"dueReminder":null,"idBoard":"5f58c9c553d5e50e89e24311","idList":"5f58c9c65854784c5402cb5b","idMembersVoted":[],"idShort":11,"idAttachmentCover":null,"idLabels":[],"manualCoverAttachment":false,"name":"Staying at home","pos":65535,"shortLink":"3gDOvH4B","isTemplate":false,"badges":{"attachmentsByType":{"trello":{"board":0,"card":0}},"location":false,"votes":0,"viewingMemberVoted":false,"subscribed":false,"fogbugz":"","checkItems":0,"checkItemsChecked":0,"checkItemsEarliestDue":null,"comments":0,"attachments":0,"description":false,"due":null,"dueComplete":false,"start":null},"dueComplete":false,"due":null,"idChecklists":[],"idMembers":[],"labels":[],"shortUrl":"https://trello.com/c/3gDOvH4B","start":null,"subscribed":false,"url":"https://trello.com/c/3gDOvH4B/11-staying-at-home","cover":{"idAttachment":null,"color":null,"idUploadedBackground":null,"size":"normal","brightness":"light"}},{"id":"5f58c9c6bf4667146a017e32","checkItemStates":null,"closed":false,"dateLastActivity":"2020-09-10T19:36:40.089Z","desc":"","descData":null,"dueReminder":null,"idBoard":"5f58c9c553d5e50e89e24311","idList":"5f58c9c65cdab23790cb8014","idMembersVoted":[],"idShort":3,"idAttachmentCover":null,"idLabels":[],"manualCoverAttachment":false,"name":"Book my holiday","pos":32768,"shortLink":"mXpbXxf9","isTemplate":false,"badges":{"attachmentsByType":{"trello":{"board":0,"card":0}},"location":false,"votes":0,"viewingMemberVoted":false,"subscribed":false,"fogbugz":"","checkItems":0,"checkItemsChecked":0,"checkItemsEarliestDue":null,"comments":0,"attachments":0,"description":false,"due":null,"dueComplete":false,"start":null},"dueComplete":false,"due":null,"idChecklists":[],"idMembers":[],"labels":[],"shortUrl":"https://trello.com/c/mXpbXxf9","start":null,"subscribed":false,"url":"https://trello.com/c/mXpbXxf9/3-book-my-holiday","cover":{"idAttachment":null,"color":null,"idUploadedBackground":null,"size":"normal","brightness":"light"}},{"id":"5f5902cd7bdb1e12c8061d9a","checkItemStates":null,"closed":false,"dateLastActivity":"2020-09-14T12:08:45.010Z","desc":"","descData":null,"dueReminder":null,"idBoard":"5f58c9c553d5e50e89e24311","idList":"5f58c9c65cdab23790cb8014","idMembersVoted":[],"idShort":4,"idAttachmentCover":null,"idLabels":[],"manualCoverAttachment":false,"name":"Fix Alicia book","pos":65536,"shortLink":"LdoweqNv","isTemplate":false,"badges":{"attachmentsByType":{"trello":{"board":0,"card":0}},"location":false,"votes":0,"viewingMemberVoted":false,"subscribed":false,"fogbugz":"","checkItems":0,"checkItemsChecked":0,"checkItemsEarliestDue":null,"comments":0,"attachments":0,"description":false,"due":null,"dueComplete":false,"start":null},"dueComplete":false,"due":null,"idChecklists":[],"idMembers":[],"labels":[],"shortUrl":"https://trello.com/c/LdoweqNv","start":null,"subscribed":false,"url":"https://trello.com/c/LdoweqNv/4-fix-alicia-book","cover":{"idAttachment":null,"color":null,"idUploadedBackground":null,"size":"normal","brightness":"light"}}]'
+    requests_mock.get('https://api.trello.com/1/search', text=board_data)
+    requests_mock.get('https://api.trello.com/1/boards/5f58c9c553d5e50e89e24311/lists', text=listinboard_data)
+    requests_mock.get('https://api.trello.com/1/boards/5f58c9c553d5e50e89e24311/cards', text=cardsinboard_data)
+    
+    assert 'Book my holiday' in client.get('/').data.decode("utf-8") 
 
-def test_get_root(client):
-    """Check a GET request to root path works"""
-    response = client.get('/')
-    assert response.status_code == 200
-    # additional response checks go here
 
-def test_app():
-    file_path = find_dotenv('.env.test')
-    load_dotenv(file_path, override=True)
-    app = Flask(__name__)
-    app.config.from_object('flask_config.Config')    
-    app.run()
-    assert app.is_running()
+# def test_cant_add_number_to_string():
+#     text = 'a'
+#     number = 3
+#     with pytest.raises(TypeError):
+#         text + number
+
+# @pytest.mark.parametrize("test_input, expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+# def test_eval(test_input, expected):
+#     assert eval(test_input) == expected
+
+# @pytest.mark.parametrize("number",range(100,150))
+# def test_is_even(number):
+#     result = number%5
+#     assert result ==0
