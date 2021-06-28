aa10a7a9b0f2e5271c16a4030304a401b543a619
Louis Egbufor <louis.egbufor@capita.com>
2021-06-28 17:11:52 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+echo "Running deployment script...push image to docker"
 docker push "$DOCKER_USERNAME"/todo-app;
+echo "Running deployment script...Azure docker pull request"
 curl -dH -X POST "$AZURE_DEPLOY_WEBHOOK"
+echo "Pushed deployment successfully"
+exit 0
