8cdadefc281b1c7f3605c77ce850b665b843612d
Louis Egbufor <louis.egbufor@capita.com>
2021-07-19 23:05:23 +0100

    added prefix default value

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "prefix" {
     description = "The prefix used for all resources in this environment"
+    default = "todo"
 }
 variable "location" {
     description = "The Azure location where all resources in this deployment should be created"
